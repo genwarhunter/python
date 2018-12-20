26c6cb00e96a79253ff095ab615671db0049fd51
genwarhunter <kostyarik98@mail.ru>
2018-12-20 10:45:37 +0500

    add function "NewForward"

--- Game testing/Cube.py
+++ Game testing/Cube.py
@@ -12,7 +12,7 @@ class CUBE(object):
         
 
 
-    def Turn_Сounterclockwise(self, flag=False):
+    def Turn_Сounterclockwise(self, edge, flag=False):
         if flag:
             self.DOWN[0].reverse
             self.UP[2].reverse
@@ -21,13 +21,13 @@ class CUBE(object):
                 self.UP[2][i], self.RIGHT[i][0] = self.RIGHT[i][0], self.UP[2][i]
                 self.DOWN[0][i], self.LEFT[i][2] = self.LEFT[i][2], self.DOWN[0][i]
 
-        return [ [self.FRONT[j][i] for j in range(3) ]for i in range(3) ]
+        return [ [edge[j][i] for j in range(3) ]for i in range(3) ]
 
 
 
-    def Turn_Clockwise(self, flag=False):
+    def Turn_Clockwise(self, edge, flag=False):
         
-        self.FRONT.reverse()
+        edge.reverse()
         
 
         if flag:
@@ -38,7 +38,7 @@ class CUBE(object):
                 self.UP[2][i], self.LEFT[i][2] = self.LEFT[i][2], self.UP[2][i]
                 self.DOWN[0][i], self.RIGHT[i][0] = self.RIGHT[i][0], self.DOWN[0][i]
 
-        return [ [self.FRONT[j][i] for j in range(3) ]for i in range(3) ]
+        return [ [edge[j][i] for j in range(3) ]for i in range(3) ]
         
 
 
@@ -53,12 +53,60 @@ class CUBE(object):
 
 
     def NewForward(self, NewF):
-        pass
+        #buff=[[ ' ' for i in range(3)]for i in range (3)]
+        if NewF == self.LEFT:
+            for i in range(3):
+                for j in range(3):
+                    self.RIGHT[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.RIGHT[i][j]
+                    self.BACK[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.BACK[i][j]
+                    self.LEFT[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.LEFT[i][j]
+            self.Turn_Clockwise(self.UP)
+            self.Turn_Сounterclockwise(self.DOWN)
+
+        elif NewF == self.RIGHT:
+            for i in range(3):
+                for j in range(3):
+                    self.LEFT[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.LEFT[i][j]
+                    self.BACK[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.BACK[i][j]
+                    self.RIGHT[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.RIGHT[i][j]
+            self.Turn_Clockwise(self.DOWN)
+            self.Turn_Сounterclockwise(self.UP)
+
+        elif NewF == self.UP:
+            for i in range(3):
+                for j in range(3):
+                    self.DOWN[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.DOWN[i][j]
+                    self.BACK[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.BACK[i][j]
+                    self.UP[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.UP[i][j]
+            self.Turn_Clockwise(self.LEFT)
+            self.Turn_Сounterclockwise(self.RIGHT)
+
+        elif NewF == self.DOWN:
+            for i in range(3):
+                for j in range(3):
+                    self.UP[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.UP[i][j]
+                    self.BACK[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.BACK[i][j]
+                    self.DOWN[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.DOWN[i][j]
+            self.Turn_Clockwise(self.RIGHT)
+            self.Turn_Сounterclockwise(self.LEFT)
+
+        elif NewF == self.BACK:
+            for i in range(2):
+                for i in range(3):
+                    for j in range(3):
+                        self.LEFT[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.LEFT[i][j]
+                        self.BACK[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.BACK[i][j]
+                        self.RIGHT[i][j], self.FRONT[i][j] = self.FRONT[i][j], self.RIGHT[i][j]
+            self.Turn_Clockwise(self.DOWN)
+            self.Turn_Сounterclockwise(self.UP)
+
+
+        
 
 if __name__ == "__main__":
     a=CUBE()
     a.output()
-    a.Turn_Clockwise()
+    a.NewForward(a.BACK)
     a.output()
 
     
